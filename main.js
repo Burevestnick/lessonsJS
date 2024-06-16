65c50104930a687e54b289257a23aed8f52122a8
Burevestnick <bychkov1058@inbox.ru>
2024-06-16 20:11:19 +0300

    homework 01

---
+++ main.js
@@ -0,0 +1,8 @@
+const title = "lesson 01"
+const screens = 1000
+const screenPrice = 1000
+const rollback = 1000
+const fullPrice = 1000
+const adaptive = 1000
+alert(title);
+console.log("Hello console!");

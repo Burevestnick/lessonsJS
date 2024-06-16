a79a7f61f2d5b6d34788baddf2919eade610fa65
Burevestnick <bychkov1058@inbox.ru>
2024-06-16 20:42:30 +0300

    подготовил начало домашки

---
+++ main.js
@@ -0,0 +1,12 @@
+const title = "lesson 01"
+const screens = "Простые, Сложные, Интерактивные"
+const screenPrice = 451
+const rollback = 74
+const fullPrice = 100000
+const adaptive = true
+
+console.log(typeof(title),typeof(fullPrice),typeof(adaptive));
+
+alert(title);
+console.log("Hello console!");
+

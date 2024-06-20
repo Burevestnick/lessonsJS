bdda1019bfcd5a10aff47b876a61cab0b7537325
Burevestnick <bychkov1058@inbox.ru>
2024-06-21 01:34:12 +0300

    homework04

--- main.js
+++ main.js
@@ -1,64 +1,85 @@
 'use strict';
 
-// 3) Спрашиваем у пользователя “Как называется ваш проект?” и результат сохраняем в переменную title
 let title = prompt("Как называется ваш проект?");
-console.log(title);
-
-// 4) Спросить у пользователя “Какие типы экранов нужно разработать?” сохранить в переменную screens (пример: "Простые, Сложные, Интерактивные")
 let screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные");
-console.log(screens);
-
-// 5) Спросить у пользователя “Сколько будет стоить данная работа?” и сохранить в переменную screenPrice (пример: 12000)
 let screenPrice = +prompt("Сколько будет стоить данная работа?");
-console.log(screenPrice);
-
 let rollback = 15
-
-
-// 6) Спросить у пользователя “Нужен ли адаптив на сайте?” и сохранить данные в переменной adaptive (булево значение true/false)
 let adaptive = confirm("Нужен ли адаптив на сайте?")
-console.log(adaptive);
-
-
-// 7) Спросить у пользователя по 2 раза каждый вопрос и записать ответы в разные переменные 1. “Какой дополнительный тип услуги нужен?” (например service1, service2) 2. “Сколько это будет стоить?” (например servicePrice1, servicePrice2) в итоге 4 вопроса и 4 разные переменных, вопросы задаются в такой последовательности Название - Стоимость - Название - Стоимость
 let service1 = prompt("Какой дополнительный тип услуги нужен?");
 let servicePrice1 = +prompt("Сколько это будет стоить?");
 let service2 = prompt("Какой дополнительный тип услуги нужен?");
 let servicePrice2 = +prompt("Сколько это будет стоить?");
+let fullPrice 
+let servicePercentPrice 
+let allServicePrices
 
 
 
-// 8) Вычислить итоговую стоимость работы учитывая стоимость верстки экранов и дополнительных услуг (screenPrice + servicePrice1 + servicePrice2) и результат занести в переменную fullPrice
-let fullPrice = screenPrice + servicePrice1 + servicePrice2
-console.log(fullPrice);
 
-
-
-// 9) Объявить переменную servicePercentPrice и занести в нее итоговую стоимость за вычетом отката посреднику (servicePercentPrice = fullPrice - Откат посреднику), округлив результат в большую сторону (методы объекта Math в помощь). Вывести servicePercentPrice в консоль.
-let servicePercentPrice = fullPrice - (fullPrice * (rollback / 100));
-console.log(Math.round(servicePercentPrice));
-
-
-
-// 10) Написать конструкцию условий (расчеты приведены в рублях) (вывести в консоль)
-//   - Если fullPrice больше 30000, то “Даем скидку в 10%” 
-//   - Если fullPrice больше 15000 и меньше 30000, то сообщение “Даем скидку в 5%” 
-//   - Если fullPrice меньше 15000 и больше 0 то в консоль вывести сообщение “Скидка не предусмотрена” 
-//   - Если отрицательное значение то вывести “Что то пошло не так” 
-//   - Учесть варианты 0, 15000 и 30000(к какому уровню не важно)
-switch (true) {
-  case fullPrice >= 30000:
-    console.log("Даем скидку в 10%");
-    break
-  case fullPrice >= 15000 && fullPrice < 30000:
-    console.log("Даем скидку в 5%");
-    break
-  case fullPrice > 0 && fullPrice < 15000:
-    console.log("Скидка не предусмотрена");
-    break
-  default:
-    console.log("Что то пошло не так");
+const getRollbackMessage = function(price) {
+  switch (true) {
+    case price >= 30000:
+     return "Даем скидку в 10%"
+    case price >= 15000 && price < 30000:
+     return "Даем скидку в 5%"
+    case price > 0 && price < 15000:
+     return "Скидка не предусмотрена"
+    default:
+     return "Что то пошло не так"
+  }
 }
+
+const getAllServicePrices = function(option1, option2) {
+  allServicePrices = option1 + option2
+  return allServicePrices
+}
+
+function getFullPrice() {
+  fullPrice = screenPrice + allServicePrices
+  return fullPrice
+}
+
+const getTitle = function(title) {
+  title = title.replace(/^\s+/, '')
+  title = title.toLowerCase() 
+  title = title[0].toUpperCase() + title.slice(1)
+  return title
+}
+
+const showTypeOf = function(option) {
+  console.log(option, typeof(option));
+}
+
+const getServicePercentPrices = function() {
+ servicePercentPrice = Math.round(fullPrice - (fullPrice * (rollback / 100))); 
+ return servicePercentPrice
+}
+
+getAllServicePrices(servicePrice1, servicePrice2);
+getFullPrice();
+
+
+showTypeOf(getTitle(title));
+showTypeOf(screenPrice);
+showTypeOf(adaptive);
+
+console.log(screens);
+console.log(getRollbackMessage(fullPrice));
+console.log(getServicePercentPrices());
+
+
+
+// 5) Почистить консоль логи и добавить недостающие, должны остаться:
+
+// - вызовы функции showTypeOf
+
+// - вывод строки с типами экранов для разработки screens
+
+// - сообщение о скидке пользователю (вызовы функции getRollbackMessage)
+
+// - стоимость за вычетом процента отката посреднику (вызовы функции getServicePercentPrices)
+
+
 
 
 

34a7373844cce4155d0e56bb663db105bfde5f53
Burevestnick <bychkov1058@inbox.ru>
2024-06-25 00:53:35 +0300

    upgrade

--- main.js
+++ main.js
@@ -1,89 +1,90 @@
- 'use strict'
+'use strict'
  
 
 
- const NumGame = function() {
+const NumGame = function() {
 
-  let current
-  let win 
-  let ask
+ let current
+ let win 
+ let ask
 
-  let tries = 10
+ let tries = 10
 
-  function randomInt(min, max) {
-    let int = min + Math.random() * (max + 1 - min);
-    return win = Math.floor(int);
-  }
-
-  function valueNull() {
-    if (current == null) {
-      alert("Игра окончена");
-    } else {
-      valueMatch();
-    }
-  }
-
-  function isNumber() {
-    return !isNaN(parseFloat(current)) && isFinite(current)
-  }
-
-  function valueSet () {
-    current = prompt("Угадайте число от 1 до 100")
-    valueNull()
-  } 
-
-  function valueMore() {
-    tries--
-    current = prompt("Загаданное число больше! У вас осталось, " + tries + " Попыток")
-    valueNull()
-  }
-
-  function valueless() {
-    tries--
-    current = prompt("Загаданное число меньше! У вас осталось, " + tries + " Попыток")
-    valueNull()
-  }
-
-  function valueError() {
-    current = prompt("Введите число!")
-    valueNull()
-  }
-
-  function valueWin() {
-    alert("Поздравляю, Вы угадали!!!");
-    ask = confirm("Хотите попробовать снова?")
-    if (ask == true) {
-      NumGame();
-    } else {
-      alert("Игра окончена")
-    }
-  }
-
-  function valueLose() {
-    ask = confirm("Попытки закончились, хотите сыграть еще?")
-    if (ask == true) {
-      NumGame();
-    } else {
-      alert("Игра окончена")
-    }
-  }
-
-  function valueMatch() {
-    if (isNumber() == false || current == '') {
-      valueError();
-    } else if (current < win && tries > 1) {
-      valueMore();
-    } else if (current > win && tries > 1) {
-      valueless();
-    } else if (current == win) {
-      valueWin();
-    } else if (tries == 1) {
-      valueLose();
-    }
-  }
-  randomInt(1, 100);
-  valueSet(); 
+ function randomInt(min, max) {
+   let int = min + Math.random() * (max + 1 - min);
+   return win = Math.floor(int);
  }
+
+ function valueNull() {
+   if (current == null) {
+     alert("Игра окончена");
+   } else {
+     valueMatch();
+   }
+ }
+
+ function isNumber() {
+   return !isNaN(parseFloat(current)) && isFinite(current)
+ }
+
+ function valueSet () {
+   alert("Бот загадал число от 1 до 100")
+   current = prompt("Угадайте это число! у вас " + tries + " Попыток")
+   valueNull()
+ } 
+
+ function valueMore() {
+   tries--
+   current = prompt("Загаданное число больше! У вас осталось, " + tries + " Попыток")
+   valueNull()
+ }
+
+ function valueless() {
+   tries--
+   current = prompt("Загаданное число меньше! У вас осталось, " + tries + " Попыток")
+   valueNull()
+ }
+
+ function valueError() {
+   current = prompt("Введите число!")
+   valueNull()
+ }
+
+ function valueWin() {
+   alert("Поздравляю, Вы угадали!!!");
+   ask = confirm("Хотите попробовать снова?")
+   if (ask == true) {
+     NumGame();
+   } else {
+     alert("Игра окончена")
+   }
+ }
+
+ function valueLose() {
+   ask = confirm("Попытки закончились, хотите сыграть еще?")
+   if (ask == true) {
+     NumGame();
+   } else {
+     alert("Игра окончена")
+   }
+ }
+
+ function valueMatch() {
+   if (isNumber() == false || current == '') {
+     valueError();
+   } else if (current < win && tries > 1) {
+     valueMore();
+   } else if (current > win && tries > 1) {
+     valueless();
+   } else if (current == win) {
+     valueWin();
+   } else if (tries == 1) {
+     valueLose();
+   }
+ }
+ randomInt(1, 100);
+ valueSet(); 
+}
 
 NumGame();
 

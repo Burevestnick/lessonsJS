793a8d4cd5074a42b036ae93f0045fe6b0a64a9c
Burevestnick <bychkov1058@inbox.ru>
2024-07-03 19:18:32 +0300

    homework07

--- main.js
+++ main.js
@@ -1,98 +1,116 @@
- 'use strict'
+'use strict';
+
+
+const AppData = {
+  title: '',
+  screens: '',
+  screenPrice: 0,
+  adaptive: true,
+  rollback: 15,
+  fullPrice: 0,
+  servicePercentPrice: 0,
+  allServicePrices: 0,
+  service1: '',
+  service2: '',
+  asking: function() {
+    AppData.title = prompt("Как называется ваш проект?", "   Калькулятор верстКи   ");
+    AppData.screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные");
+    while (AppData.screens == "" || AppData.screens === null) {
+      AppData.screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные");
+    }
+     do {
+      AppData.screenPrice = prompt("Сколько будет стоить данная работа?", 25000);
+      AppData.screenPrice = parseFloat(AppData.screenPrice)
+    } while(!isNumber(AppData.screenPrice) || AppData.screenPrice === null );
+  
+    AppData.adaptive = confirm("Нужен ли адаптив на сайте?")
+  },
+  logger: function() {
+    console.log(AppData.fullPrice);
+    console.log(AppData.servicePercentPrice);
+    for (let key in AppData) {
+      console.log("key: " + key + " " + "value: " + " " + AppData[key]); 
+    }
+  },
+  start: function() {
+    AppData.asking();
+    AppData.allServicePrices = getAllServicePrices();
+    AppData.fullPrice = getFullPrice();
+    AppData.servicePercentPrice = getServicePercentPrices();
+    AppData.title = getTitle(AppData.title);
+    AppData.logger()
+  }
+}
+
+const isNumber = function(num) {
+  return !isNaN(parseFloat(num)) && isFinite(num)
+}
+
+const getAllServicePrices = function() {
+  let current = 0
+  let sum = 0
+  for (let i = 0; i < 2; i++) {
+    if (i === 0) {
+      AppData.service1 = prompt("Какой дополнительный тип услуги нужен?", "addition");
+      while (AppData.service1 === null || AppData.service1 == "") {
+        AppData.service1 = prompt("Какой дополнительный тип услуги нужен?", "addition");
+      }
+    } else if (i === 1) {
+      AppData.service2 = prompt("Какой дополнительный тип услуги нужен?", "addition");
+      while (AppData.service2 === null || AppData.service2 == "") {
+        AppData.service2 = prompt("Какой дополнительный тип услуги нужен?", "addition");
+      }
+    }
+
+    do {
+      current = prompt("Сколько будет стоить данная работа?", 15000);
+      current = parseFloat(current)
+    } while(!isNumber(current) || current === null );
+    sum = sum + current
+  }
+  return sum
+}
+
+const getRollbackMessage = function(price) {
+  switch (true) {
+    case price >= 30000:
+     return "Даем скидку в 10%"
+    case price >= 15000 && price < 30000:
+     return "Даем скидку в 5%"
+    case price > 0 && price < 15000:
+     return "Скидка не предусмотрена"
+    default:
+     return "Что то пошло не так"
+  }
+}
+
+
+const getFullPrice =  function() {
+  return AppData.screenPrice + AppData.allServicePrices
+}
+
+const getTitle = function(title) {
+  title = title.trim().toLowerCase() 
+  title = title[0].toUpperCase() + title.slice(1)
+  return title
+}
+
+const getServicePercentPrices = function() {
+ return AppData.fullPrice - (AppData.fullPrice * (AppData.rollback / 100)); 
+}
+
+
+AppData.start()
+
+
+
  
 
 
- const NumGame = function() {
 
-  let current
-  let win 
-  let ask
 
-  let tries = 10
 
-  function randomInt(min, max) {
-    let int = min + Math.random() * (max + 1 - min);
-    return win = Math.floor(int);
-  }
 
-  function valueNull() {
-    if (current == null) {
-      alert("Игра окончена");
-    } else {
-      valueMatch();
-    }
-  }
 
-  function isNumber() {
-    return !isNaN(parseFloat(current)) && isFinite(current)
-  }
 
-  function valueSet () {
-    alert("Бот загадал число от 1 до 100")
-    current = prompt("Угадайте это число! у вас " + tries + " Попыток")
-    valueNull()
-  } 
-
-  function valueMore() {
-    tries--
-    current = prompt("Загаданное число больше! У вас осталось, " + tries + " Попыток")
-    valueNull()
-  }
-
-  function valueless() {
-    tries--
-    current = prompt("Загаданное число меньше! У вас осталось, " + tries + " Попыток")
-    valueNull()
-  }
-
-  function valueError() {
-    current = prompt("Введите число!")
-    valueNull()
-  }
-
-  function valueWin() {
-    alert("Поздравляю, Вы угадали!!!");
-    ask = confirm("Хотите попробовать снова?")
-    if (ask == true) {
-      NumGame();
-    } else {
-      alert("Игра окончена")
-    }
-  }
-
-  function valueLose() {
-    ask = confirm("Попытки закончились, хотите сыграть еще?")
-    if (ask == true) {
-      NumGame();
-    } else {
-      alert("Игра окончена")
-    }
-  }
-
-  function valueMatch() {
-    if (isNumber() == false || current == '') {
-      valueError();
-    } else if (current < win && tries > 1) {
-      valueMore();
-    } else if (current > win && tries > 1) {
-      valueless();
-    } else if (current == win) {
-      valueWin();
-    } else if (tries == 1) {
-      valueLose();
-    }
-  }
-  randomInt(1, 100);
-  valueSet(); 
- }
-
-NumGame();
-
-// Кол-во попыток пользователя должно быть ограничено: 10
-// — если пользовательское число больше, то бот выводит "Загаданное число меньше, осталось попыток ..." и предлагает ввести новый вариант;
-// — если пользовательское число меньше, то бот выводит "Загаданное число больше, осталось попыток ..." и предлагает ввести новый вариант;
-// — если пользователь вводит правильное число, то бот выводит "Поздравляю, Вы угадали!!! Хотели бы сыграть еще?", при нажатии ОК игра перезапускается (снова 10 попыток и новое загаданное число)
-// — если пользователь ввел не число, то выводит сообщение "Введи число!" и предлагает ввести новый вариант;
-// — если пользователь нажимает "Отмена", то игра выводит прощальное сообщение и завершается.
-// — если закончились попытки то программа сообщает: "Попытки закончились, хотите сыграть еще?"
 
